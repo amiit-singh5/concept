24a196899453f3272a2dad8a500830e80969bfe9
amiit-singh5 <amiit.singh50@gmail.com>
2026-02-25 17:29:02 +0530

    without sum function usage
    Removed the use of the sum function and added a comment about the implementation.

--- lmbda1.py
+++ lmbda1.py
@@ -1,3 +1,4 @@
+# without using sum function adding all the elements of list
 # sum without sum and reduce from functools using list comprehesion
 nums = [1,2,3,4]
 print("1. using sum : ",sum(nums))  # replacing this function
