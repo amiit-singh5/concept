946140d145572a062ed25e4ca36b71b864a33c60
amiit-singh5 <amiit.singh50@gmail.com>
2026-02-25 15:23:01 +0530

    initial commit from vscode

--- lmbda1.py
+++ lmbda1.py
@@ -1,9 +1,13 @@
 # sum without sum and reduce from functools using list comprehesion
 nums = [1,2,3,4]
+print("1. using sum : ",sum(nums))  # replacing this function
+
+# adding all members of list without using sum function
 total = 0
 a = [total := total + x  for x in nums]
 
-print(total)
+print("2. Addition without sum : ",total)
+
 
 
 
@@ -12,5 +16,5 @@ print(total)
 
 # find max in nums = [1,2,3,4] using lambda
 
-fun = lambda x : 
-print([(lambda i, temp = nums[0]: (temp := i) if (i > temp) else temp)(i) for i in nums])
+# fun = lambda x : 
+# print([(lambda i, temp = nums[0]: (temp := i) if (i > temp) else temp)(i) for i in nums])
