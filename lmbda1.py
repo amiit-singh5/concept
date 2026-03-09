f9fb86013a2875614ff48d72571f8fe883d02971
amiit-singh5 <amiit.singh50@gmail.com>
2026-03-09 17:15:04 +0530

    branch b2 has added comment in the file

--- lmbda1.py
+++ lmbda1.py
@@ -6,7 +6,8 @@ print("1. using sum : ",sum(nums))  # replacing this function
 # adding all members of list without using sum function
 total = 0
 a = [total := total + x  for x in nums]
-
+# brach b2 has added comment
 print("2. Addition without sum : ",total)
+print(" Branch b2 comment ")
 
 
