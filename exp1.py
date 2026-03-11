75965843f42a85c04c5898b4f1547982cae32ef8
amiit-singh5 <amiit.singh50@gmail.com>
2026-03-11 15:22:54 +0530

    all dev need to sync with main

---
+++ exp1.py
@@ -0,0 +1,7 @@
+# this is file for experimentation
+# having 2 branch b1 and b2
+# stages 3 , 
+# 1. Stash after add and before commit , 
+# 2. You have added, comitted but not pushed
+# 3. You have added, commited and pused  
+print("New module have been added, instruction to all the delepoper is to syc from main")
